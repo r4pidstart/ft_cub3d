dbc319868a5f8d6d1efdeafb07987bc5cf0737b0
r4pidstart <r4pidstart@gmail.com>
2023-01-30 04:23:52 +0900

    separation keyhook functions

--- cub3d/ft_header.h
+++ cub3d/ft_header.h
@@ -6,7 +6,7 @@
 /*   By: tjo <tjo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 02:29:32 by tjo               #+#    #+#             */
-/*   Updated: 2023/01/20 04:43:27 by tjo              ###   ########.fr       */
+/*   Updated: 2023/01/30 04:21:53 by tjo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 # define FT_HEADER_H
 
 # include"raycasting/ft_raycasting_header.h"
+
+/* ft_keyhook.c */
+int	check_collision(t_param *t, double next_x, double next_y);
+int	move_player(int key, t_param *t);
+int	close_game(void);
+int	key_hook(int key, t_param *t);
 
 #endif
--- cub3d/ft_main.c
+++ cub3d/ft_main.c
@@ -6,7 +6,7 @@
 /*   By: tjo <tjo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 02:29:09 by tjo               #+#    #+#             */
-/*   Updated: 2023/01/30 04:19:16 by tjo              ###   ########.fr       */
+/*   Updated: 2023/01/30 04:20:29 by tjo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,68 +73,6 @@ int worldMap[24][24]=
   {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
-
-int	check_collision(t_param *t, double next_x, double next_y)
-{
-	const double	safe_dist = 0.2;
-
-	if (t->map->map[(int)(next_x + safe_dist)][(int)(next_y + safe_dist)] \
-		|| t->map->map[(int)(next_x - safe_dist)][(int)(next_y - safe_dist)] \
-		|| t->map->map[(int)(next_x - safe_dist)][(int)(next_y + safe_dist)] \
-		|| t->map->map[(int)(next_x + safe_dist)][(int)(next_y - safe_dist)])
-		return (0);
-	t->map->player_x = next_x;
-	t->map->player_y = next_y;
-	return (0);
-}
-
-// TODO: wall collision, exit button
-int	move_player(int key, t_param *t)
-{
-	const double	move_speed = 0.07;
-	double			next_x;
-	double			next_y;
-
-	if (key == K_W)
-	{
-		next_x = t->map->player_x + t->map->dir_x * move_speed;
-		next_y = t->map->player_y + t->map->dir_y * move_speed;
-	}
-	else if (key == K_A)
-	{
-		next_x = t->map->player_x - t->map->dir_x * move_speed;
-		next_y = t->map->player_y + t->map->dir_y * move_speed;
-	}
-	else if (key == K_S)
-	{
-		next_x = t->map->player_x - t->map->dir_x * move_speed;
-		next_y = t->map->player_y - t->map->dir_y * move_speed;
-	}
-	else // if (key == K_D)
-	{
-		next_x = t->map->player_x + t->map->dir_x * move_speed;
-		next_y = t->map->player_y - t->map->dir_y * move_speed;
-	}
-	return (check_collision(t, next_x, next_y));
-}
-
-int	close_game(void)
-{
-	exit(0);
-}
-
-int	key_hook(int key, t_param *t)
-{
-	if (key == K_LEFT || key == K_RIGHT)
-		change_dir(key, t->map, 0.07);
-	else if (key == K_W || key == K_A || key == K_S || key == K_D)
-		move_player(key, t);
-	else if (key == K_ESC)
-		exit(0);
-	mlx_clear_window(t->mlx->m, t->mlx->w);
-	looooop(t);
-	return (0);
-}
 
 int	main(void)
 {
